f9f091ffde8e84ff8ae7775e7d802c90c6d642de
徐涛 <xutao@TennyXu.local>
2017-07-07 11:15:13 +0800

    Friday, 7 July 2017 at 11:15:04

---
+++ DemoSingleViewApptoNavigationViewApp/DemoSingleViewApptoNavigationViewApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DemoSingleViewApptoNavigationViewApp
+//
+//  Created by 徐涛 on 07/07/2017.
+//  Copyright © 2017 徐涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.white
+        self.title = "DemoSingleViewApptoNavigationViewApp"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DemoSingleViewApptoNavigationViewApp
-将iOS  Single View App 转换为iOS Navigation View App
+将iOS  Single View App 转换为iOS Navigation View App（代码方式）
+
+方式1：代码方式，步骤如下：
+1. 在AppDelegate.swift中设置根视图为UINavigationController
+let rootViewController = ViewController()
+let rootNavigationController = UINavigationController(rootViewController:rootViewController)
+self.window!.rootViewController = rootNavigationController
+
+2. 修改ViewController的背景颜色（SB默认的背景颜色为黑色）
+self.view.backgroundColor = UIColor.white
+
+3. 添加标题
+self.title = "DemoSingleViewApptoNavigationViewApp"
+
+3. 运行结果显示，Single View App已经转换为了Navigation View App
